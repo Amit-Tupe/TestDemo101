bc4f9fb1467c70831e322a69813c01da0b7a59dc
AMITRAJE <AMITRAJE@192.168.1.204>
2020-12-23 12:16:55 +0530

    add method

--- Gitdemo101/src/com/cjc/helloword.java
+++ Gitdemo101/src/com/cjc/helloword.java
@@ -2,6 +2,10 @@ package com.cjc;
 
 public class helloword 
 {
+	public void dislplay()
+	{
+		System.out.println("dislplay");
+	}
 public static void main(String[] args) 
 {
 	System.out.println("Hello git");
